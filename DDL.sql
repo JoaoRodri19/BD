214fd4008106b78631f71607ac79f78b9f162791
JoaoRodrigues08 <joaopprodrigues08@gmail.com>
2023-05-02 01:25:15 +0100

    all constraints done

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA ds3;
+
 
 CREATE TABLE ds3.Zona(
 Nome varchar(20) NOT NULL PRIMARY KEY,
@@ -10,7 +10,7 @@ Item int,
 Zona varchar(20),
 Coordenadas varchar(20) NOT NULL PRIMARY KEY
 );
-
+ 
 CREATE TABLE ds3.Localizacao_Adversário(
 Adversario int,
 Localizacao varchar(20),
@@ -30,8 +30,10 @@ Pontos_De_Vida int,
 ID int PRIMARY KEY NOT NULL
 );
 
+
+
 CREATE TABLE ds3.Jogador(
-Persongem int PRIMARY KEY,
+Personagem int PRIMARY KEY,
 Item_Discovery int,
 Stamina int,
 Classe varchar(20),
@@ -48,11 +50,11 @@ Valor int
 );
 
 CREATE TABLE ds3.Adversario(
-Moveset
-Drops
-Fraquezas
-Resistencias
-Imunidades
+Moveset			varchar(30),
+Drops			varchar(30),
+Fraquezas		varchar(30),
+Resistencias	varchar(30),
+Imunidades		varchar(30),
 Personagem int PRIMARY KEY
 );
 
@@ -69,8 +71,8 @@ Adversario int PRIMARY KEY
 
 CREATE TABLE ds3.NPC(
 Dialogo text,
-Adversario int PRIMARY KEY
-Quest text
+Adversario int PRIMARY KEY,
+Quest int
 );
 
 CREATE TABLE ds3.Quest(
@@ -118,6 +120,41 @@ Jogador int,
 Atributos int
 PRIMARY KEY(Jogador,Atributos)
 );
+
+alter table ds3.Localizacao add constraint fk_localizacao_Item foreign key (Item) references  ds3.Item (ID);
+alter table ds3.Localizacao add constraint fk_localizacao_Zona foreign key (Zona) references  ds3.Zona (Nome);
+
+alter table ds3.Localizacao_Adversário add constraint fk_localizacao_adversario_localizacao foreign key (Localizacao) references  ds3.Localizacao (Coordenadas);
+alter table ds3.Localizacao_Adversário add constraint fk_localizacao_adversario_adversario foreign key (Adversario) references  ds3.Adversario (Personagem);
+
+alter table ds3.Saves add constraint fk_Saves_Ultima_localizacao foreign key (Ultima_localizacao) references  ds3.Localizacao (Coordenadas);
+alter table ds3.Saves add constraint fk_Saves_Jogador foreign key (Jogador) references  ds3.Jogador (Personagem);
+
+alter table ds3.Adversario add constraint fk_Adversario_Personagem foreign key (Personagem) references  ds3.Personagem (ID);
+
+alter table ds3.Inimigo add constraint fk_Inimigo_Adversario foreign key (Adversario) references ds3.Adversario (Personagem);
+
+alter table ds3.Boss add constraint fk_Boss_Adversario foreign key (Adversario) references ds3.Adversario (Personagem);
+
+alter table ds3.NPC add constraint fk_NPC_Adversario foreign key (Adversario) references ds3.Adversario (Personagem);
+
+alter table ds3.Quest add constraint fk_Quest_NPC foreign key (NPC) references ds3.NPC (Adversario);
+
+alter table ds3.Item_Personagem add constraint fk_Item_Personagem_Item foreign key (Item) references ds3.Item (ID);
+alter table ds3.Item_Personagem add constraint fk_Item_Personagem_Personagem foreign key (Personagem) references ds3.Personagem (ID);
+
+alter table ds3.Item_Equipavel add constraint fk_Item_Equipavel_Item foreign key (Item) references ds3.Item (ID);
+
+alter table ds3.Arma add constraint fk_Arma_Item_Equipavel foreign key (Item_Equipavel) references ds3.Item_Equipavel(Item);
+
+alter table ds3.Armadura add constraint fk_Armadura_Item_Equipavel foreign key (Item_Equipavel) references ds3.Item_Equipavel(Item);
+
+alter table ds3.Item_Nao_Equipavel add constraint fk_Item_Nao_Equipavel_Item foreign key (Item) references ds3.Item (ID);
+
+alter table ds3.Jogador_Atributos add constraint fk_Jogador_Atributos_Jogador foreign key (Jogador) references ds3.Jogador (Personagem);
+alter table ds3.Jogador_Atributos add constraint fk_Jogador_Atributos_Atributos foreign key (Atributos) references ds3.Atributos (ID);
+
+
 
 
 
