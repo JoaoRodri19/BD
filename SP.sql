1f3e41ae845ae94595ce289543e40bdafe57b169
JoaoRodrigues08 <joaopprodrigues08@gmail.com>
2023-05-30 18:23:32 +0100

    sp added

--- SP.sql
+++ SP.sql
@@ -1,10 +1,38 @@
-SELECT * FROM ds3.Boss
-JOIN ds3.Adversario on Boss.Adversario = Adversario.Personagem 
-JOIN ds3.Personagem on ID = Adversario.Personagem 
-JOIN ds3.Localizacao_Adversario on Adversario.Personagem = Localizacao_Adversario.Adversario
-JOIN ds3.Localizacao on Localizacao.coordenadas =  Localizacao_Adversario.coordenadas
-JOIN ds3.Zona on Zona.Nome = Localizacao.Zona
+CREATE PROCEDURE GetBossInformation
+AS
+BEGIN
+  SELECT
+    Personagem.nome AS "Boss Name",
+    Pontos_De_Vida AS "HP",
+    Zona.nome AS "Zona"
+  FROM
+    ds3.Boss
+    JOIN ds3.Adversario ON Boss.Adversario = Adversario.Personagem
+    JOIN ds3.Personagem ON ID = Adversario.Personagem
+    JOIN ds3.Localizacao_Adversario ON Adversario.Personagem = Localizacao_Adversario.Adversario
+    JOIN ds3.Localizacao ON Localizacao.Coordenadas = Localizacao_Adversario.Localizacao
+    JOIN ds3.Zona ON Zona.Nome = Localizacao.Zona;
+END;
+GO
+CREATE PROCEDURE GetBossByHp @hp_ini int, @hp_fim int
+AS
+BEGIN
+SELECT
+    Personagem.nome AS "Boss Name",
+    Pontos_De_Vida AS "HP",
+    Zona.nome AS "Zona"
+  FROM
+    ds3.Boss
+    JOIN ds3.Adversario ON Boss.Adversario = Adversario.Personagem
+    JOIN ds3.Personagem ON ID = Adversario.Personagem
+    JOIN ds3.Localizacao_Adversario ON Adversario.Personagem = Localizacao_Adversario.Adversario
+    JOIN ds3.Localizacao ON Localizacao.Coordenadas = Localizacao_Adversario.Localizacao
+    JOIN ds3.Zona ON Zona.Nome = Localizacao.Zona
+	where Pontos_De_Vida > @hp_ini and Pontos_De_Vida < @hp_fim
+END;
+GO
 
-
-
-
+-- drop sp
+DROP PROCEDURE IF EXISTS GetBossInformation;
+exec GetBossInformation
+exec GetBossByHp @hp_ini = 1, @hp_fim = 2000
